05700d781625d43066fbc1b4b336b4a6de4cc1b1
Daniel Ran <danielr1735@gmail.com>
2018-07-17 15:51:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    @string = params[:user_text]
+    @word_count = @string.split(' ').length
+    @vowel_count = @string.scan(/[aeiou]/).count
+    @consonant_count = @string.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+
+    letter_count = Hash.new 0
+    @string.split('').each { |c| letter_count[c] += 1 }
+    @most_used_letter = letter_count.max_by {|k,v| v }
 
     erb :results
   end
