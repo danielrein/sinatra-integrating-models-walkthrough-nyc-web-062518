6fcfbe2aabd42b8ecbe6603d829e9964f3290ec8
Daniel Ran <danielr1735@gmail.com>
2018-07-17 16:04:36 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,27 +1,27 @@
 # Your TextAnalyzer model code will go here.
 
 class TextAnalyzer
-  attr_reader :string
+  attr_reader :text
 
-  def initialize(string)
-    @string = string.downcase
+  def initialize(text)
+    @text = text.downcase
   end
 
   def count_of_words
-    string.split(' ').length
+    text.split(' ').length
   end
 
   def count_of_vowels
-    string.scan(/[aeiou]/).count
+    text.scan(/[aeiou]/).count
   end
 
   def count_of_consonants
-    string.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
 
   def most_used_letter
     letter_count = Hash.new 0
-    clean = string.gsub(/[^a-z]/, '')
+    clean = text.gsub(/[^a-z]/, '')
     clean.split('').each { |c| letter_count[c] += 1 }
     letter_count.max_by {|k,v| v }
   end
