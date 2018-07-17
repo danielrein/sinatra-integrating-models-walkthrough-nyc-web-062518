7195b290cccc76db00bee4394a7317e14c761cf5
Daniel Ran <danielr1735@gmail.com>
2018-07-17 16:00:06 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,18 +1,28 @@
 # Your TextAnalyzer model code will go here.
 
 class TextAnalyzer
-  attr_reader :text
+  attr_reader :string
 
-def initialize(text)
-  @text = text
-end
-@string = params[:user_text]
-@word_count = @string.split(' ').length
-@vowel_count = @string.scan(/[aeiou]/).count
-@consonant_count = @string.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  def initialize(string)
+    @string = string.downcase
+  end
 
-letter_count = Hash.new 0
-@string.split('').each { |c| letter_count[c] += 1 }
-@most_used_letter = letter_count.max_by {|k,v| v }
+  def count_of_words
+    @string.split(' ').length
+  end
+
+  def count_of_vowels
+    @string.scan(/[aeiou]/).count
+  end
+
+  def count_of_consonants
+    @string.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+    letter_count = Hash.new 0
+    @string.split('').each { |c| letter_count[c] += 1 }
+    letter_count.max_by {|k,v| v }
+  end
 
 end
